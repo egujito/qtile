697c4dc8d1c3cfadac8b043be7e47c642cc88727
egujito <bessapachecolr@gmail.com>
2023-06-19 12:08:45 +0100

    Updated the bar

--- bar1.py
+++ bar1.py
@@ -9,6 +9,14 @@ widget_defaults = dict(
 
 
 extension_defaults = widget_defaults.copy()
+global_pad = 10
+# separator settings, act as global config for bar separators
+
+def init_sep():
+    return widget.Sep(
+                size_percent=50,
+                padding=global_pad
+            )
 
 screens = [
     Screen(
@@ -26,8 +34,54 @@ screens = [
                 #),
                 # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
                 # widget.StatusNotifier(),
+                init_sep(),
+
+                widget.CheckUpdates(
+                    distro="Arch",
+                    no_update_string="0 updates",
+                    fmt="󰮯  {}",
+                    padding=global_pad,
+                    ),
+
+                init_sep(),
+
+                widget.Battery(
+                    battery=0,
+                    charge_char="",
+                    discharge_char="󰉃",
+                    full_char=" ",
+                    empty_char=" ",
+                    format='{char} {percent:2.0%}',
+                    padding=global_pad
+                    ),
+
+                init_sep(), 
+
+                widget.Volume(
+                    fmt=" : {}",
+                    emoji=False,
+                    padding=global_pad,
+                    scroll=True
+                    ),
+
+                init_sep(),
+                
+                #time
+                widget.Clock(
+                    format=" %I:%M %p",
+                    padding=global_pad
+                    ),
+                
+                init_sep(),
+                #date
+                widget.Clock(
+                    format=" %Y-%m-%d %a",
+                    padding=global_pad
+                    ),
+
+                init_sep(),
+
                 widget.Systray(),
-                widget.Clock(format=" %I:%M %p |  %Y-%m-%d %a"),
             ],
             40,
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
